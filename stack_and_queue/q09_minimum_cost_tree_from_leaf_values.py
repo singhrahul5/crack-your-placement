8a5a0b9e7e10d492e8ea2ad97dbb004bde14f7eb
Rahul Singh <imrahulkumar590@gmail.com>
2024-08-19 17:21:26 +0530

    solve q 9 of stack

--- stack_and_queue/q09_minimum_cost_tree_from_leaf_values.py
+++ stack_and_queue/q09_minimum_cost_tree_from_leaf_values.py
@@ -1,56 +1,71 @@
 from typing import List
 
 
-def mct_from_leaf_values(self, arr: List[int]) -> int:
-    # dp = [[None for _ in arr] for _ in arr]
+class Solution:
+    def using_stack(self, arr):
+        res = 0
+        stack = [float('inf')]
 
-    # return self.solve(0, len(arr) - 1, arr, dp)[1]
-    return self.tab(arr)
+        for ele in arr:
+            while stack[-1] <= ele:
+                mid = stack.pop()
+                res += mid * min(stack[-1], ele)
+            stack.append(ele)
 
+        while len(stack) > 2:
+            res += stack.pop() * stack[-1]
 
-def tab(self, arr):
-    n = len(arr)
-    max_arr = []
-    for i in range(n):
-        sub = [0] * n
-        sub[i] = arr[i]
-        for j in range(i + 1, n):
-            sub[j] = max(sub[j - 1], arr[j])
+        return res
 
-        max_arr.append(sub)
+    def mct_from_leaf_values(self, arr: List[int]) -> int:
+        # dp = [[None for _ in arr] for _ in arr]
 
-    dp = [[0] * n for _ in range(n)]
+        # return self.solve(0, len(arr) - 1, arr, dp)[1]
+        # return self.tab(arr)
+        return self.using_stack(arr)
 
-    for left in range(n - 1, -1, -1):
-        for right in range(left + 1, n):
-            ans = float('inf')
-            for div in range(left, right):
-                ans = min(ans, max_arr[left][div] * max_arr[div + 1][right] + dp[left][div] + dp[div + 1][right])
-            dp[left][right] = ans
+    def tab(self, arr):
+        n = len(arr)
+        max_arr = []
+        for i in range(n):
+            sub = [0] * n
+            sub[i] = arr[i]
+            for j in range(i + 1, n):
+                sub[j] = max(sub[j - 1], arr[j])
 
-    return dp[0][n - 1]
+            max_arr.append(sub)
 
+        dp = [[0] * n for _ in range(n)]
 
-def solve(self, left: int, right: int, arr: List[int], dp: List[List[any]]) -> List[int]:
-    if left == right:
-        return [arr[left], 0]
+        for left in range(n - 1, -1, -1):
+            for right in range(left + 1, n):
+                ans = float('inf')
+                for div in range(left, right):
+                    ans = min(ans, max_arr[left][div] * max_arr[div + 1][right] + dp[left][div] + dp[div + 1][right])
+                dp[left][right] = ans
 
-    if dp[left][right] is not None:
+        return dp[0][n - 1]
+
+    def solve(self, left: int, right: int, arr: List[int], dp: List[List[any]]) -> List[int]:
+        if left == right:
+            return [arr[left], 0]
+
+        if dp[left][right] is not None:
+            return dp[left][right]
+
+        total = float('inf')
+        max_leaf = 0
+
+        for div in range(left, right):
+            left_child = self.solve(left, div, arr, dp)
+            right_child = self.solve(div + 1, right, arr, dp)
+
+            if left_child[1] + right_child[1] + left_child[0] * right_child[0] < total:
+                total = left_child[1] + right_child[1] + left_child[0] * right_child[0]
+                # print(left, div, right, sum)
+                max_leaf = max(left_child[0], right_child[0])
+
+        dp[left][right] = [max_leaf, total]
+
+        # print(left, right, dp[left][right])
         return dp[left][right]
-
-    total = float('inf')
-    max_leaf = 0
-
-    for div in range(left, right):
-        left_child = self.solve(left, div, arr, dp)
-        right_child = self.solve(div + 1, right, arr, dp)
-
-        if left_child[1] + right_child[1] + left_child[0] * right_child[0] < total:
-            total = left_child[1] + right_child[1] + left_child[0] * right_child[0]
-            # print(left, div, right, sum)
-            max_leaf = max(left_child[0], right_child[0])
-
-    dp[left][right] = [max_leaf, total]
-
-    # print(left, right, dp[left][right])
-    return dp[left][right]
